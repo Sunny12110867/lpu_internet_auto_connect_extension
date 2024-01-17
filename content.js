91fb442d7b6bf2549b0d4e4928eed1626900eaf5
sunny bhardwaj <104976449+Sunny12110867@users.noreply.github.com>
2024-01-17 17:01:37 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,6 +1,4 @@
-// content.js
 
-// Function to simulate clicking the "I Agree with Terms and Conditions" checkbox
 console.log("yes it is starting ");
 function agreeWithTerms() {
     var agreeCheckbox = document.getElementById('agreepolicy');
@@ -9,30 +7,28 @@ function agreeWithTerms() {
     }
   }
   
-  // Function to fill in the username and password fields
+
   function fillLoginForm() {
     var usernameField = document.querySelector('input[name="username"]');
     var passwordField = document.querySelector('input[name="password"]');
   
     if (usernameField && passwordField) {
-      // Replace 'your_username' and 'your_password' with actual values
-      usernameField.value = '12110867';
-      passwordField.value = '8082746001';
+     
+      usernameField.value = '';
+      passwordField.value = '';
   
-      // Enable the login button
       enableLoginButton();
     }
   }
   
-  // Function to enable the login button
+
   function enableLoginButton() {
     var loginButton = document.getElementById('loginbtn');
     if (loginButton) {
       loginButton.removeAttribute('disabled');
     }
   }
-  
-  // Function to click the login button
+
   function clickLoginButton() {
     var loginButton = document.getElementById('loginbtn');
     if (loginButton) {
@@ -40,7 +36,7 @@ function agreeWithTerms() {
     }
   }
   
-  // Call the functions to automate the login process
+  
   agreeWithTerms();
   fillLoginForm();
   clickLoginButton();
